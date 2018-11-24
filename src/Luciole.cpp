079d57ddb878587d49a78b4e2bc024d0131fcec7
Cyril <brebisasthmatique@gmail.com>
2018-11-24 21:19:30 +0100

    Correction de la lumière de la luciole

--- src/Luciole.cpp
+++ src/Luciole.cpp
@@ -34,7 +34,7 @@ void Luciole::mouv()
 		abs += vitesse * tps*cos(dir);
 		ord += vitesse * tps*sin(dir);
 
-		lights.emplace_back(abs, ord);
+		lights.emplace_back(abs + 10, ord + 10);
 		lights.back().setAlphaSpeed(600);
 		lights.back().setAlpha(255);
 		lights.back().setRadiusSpeed(0);
