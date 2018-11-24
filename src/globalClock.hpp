d01bde683562ddaa6a97ce387b3090eb0b5778a9
paul.lemeur <paulemeur@gmail.com>
2018-11-24 22:14:16 +0100

    Pierre pue la merde

--- src/globalClock.hpp
+++ src/globalClock.hpp
@@ -22,7 +22,7 @@ class globalClock
         
 
     private:
-        sf::Clock clock;"'eé"
+        sf::Clock clock;
         sf::Time frame = sf::Time::Zero;
         sf::Time global = sf::Time::Zero;
         double factor = 1;
