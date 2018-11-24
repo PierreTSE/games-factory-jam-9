f66c01e82c452e48e75d22dd09511990910a1650
PierreTSE <pierre--giraud@orange.fr>
2018-11-24 22:02:14 +0100

    ajout main pour tester la classe Environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,76 @@
-#include <SFML/Graphics.hpp>
-#include <vector>
-
-#include "Character.h"
 #include "AnimatedEntity.hpp"
+#include "Character.h"
+#include "constantes.hpp"
+#include "environment/Environment.hpp"
 #include "globalClock.hpp"
+#include "Luciole.h"
 #include "RessourceLoader.hpp"
-#include"Luciole.h"
-#include"constantes.hpp"
 #include "SoundWave.h"
+
+#include <filesystem>
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+
+namespace fs = std::filesystem;
+
+void mainTestEnvironment(fs::path rc)
+{
+    Environment environment;
+    environment.load(rc / "map" / "map10.png");
+
+    const float window_x = 800.f, window_y = 700.f;
+
+    sf::RenderWindow window(sf::VideoMode(window_x, window_y),
+                            "Test Environment",
+                            sf::Style::Default,
+                            sf::ContextSettings(0, 0, 8));
+
+    const float ratio = 10.f;
+
+	const sf::RectangleShape background({window_x, window_y});
+
+
+    while(window.isOpen())
+    {
+        sf::Event event{};
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+            if(event.type == sf::Event::KeyPressed)
+            {
+                switch(event.key.code)
+                {
+                    case sf::Keyboard::Space :
+                        environment.switchPillars();
+                        break;
+                }
+            }
+        }
+
+        globalClock::getClock().restart();
+
+        window.clear();
+
+        window.draw(background);
+        environment.drawObstacles(window, ratio);
+
+        window.display();
+
+        sf::sleep(sf::milliseconds(10));
+    }
+
+}
 
 
 int main()
 {
+    const fs::path rc = "../rc";
+
+    //mainTestEnvironment(rc);
+
+
     // Ce code peux servir à faire des changements automatiques sur les sprites, à garder
     //for(auto path : {"../../rc/sprites/ringing_left.png"})
     //{
@@ -41,14 +100,16 @@ int main()
 
     Player monPerso;
 
-	Luciole luciole;
-	luciole.set_coordd(500, 500);
-	luciole.set_coordf(100, 100);
+    Luciole luciole;
+    luciole.set_coordd(500, 500);
+    luciole.set_coordf(100, 100);
 
-	std::vector<SoundWave> waves;
+    std::vector<SoundWave> waves;
 
     //Création de la fenetre du jeux
-    sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y), "SUPER BIZUT", sf::Style::Default,
+    sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y),
+                            "SUPER BIZUT",
+                            sf::Style::Default,
                             sf::ContextSettings(0, 0, 8));
 
     //Tant que l'on joue (fenetre ouverte)
@@ -56,8 +117,6 @@ int main()
     {
         //Création d'un objet récupérant les événements (touche clavier et autre)
         sf::Event event{};
-
-
 
 
         //Boucle des évennements
@@ -70,10 +129,10 @@ int main()
             {
                 switch(event.key.code)
                 {
-                    case sf::Keyboard::Space:
+                    case sf::Keyboard::Space :
                         monPerso.ring();
-						if((waves.size() == 0 || waves.back().getTime() > sf::milliseconds(500)))
-							waves.emplace_back(monPerso.getPosition().x, monPerso.getPosition().y);
+                        if((waves.size() == 0 || waves.back().getTime() > sf::milliseconds(500)))
+                            waves.emplace_back(monPerso.getPosition().x, monPerso.getPosition().y);
                         break;
                 }
             }
@@ -82,34 +141,31 @@ int main()
         globalClock::getClock().restart();
 
 
-        monPerso.movement(window, globalClock::getClock().frameTime());//Mouvement du personnage
+        monPerso.movement(window, globalClock::getClock().frameTime()); //Mouvement du personnage
 
         window.clear();
 
-		monPerso.draw(window);
+        monPerso.draw(window);
 
-		luciole.mouv();
-		luciole.draw(window);
+        luciole.mouv();
+        luciole.draw(window);
 
-		for (size_t i = 0; i < waves.size(); i++)
-		{
-			waves[i].update();
-			waves[i].draw(window);
-		}
+        for(size_t i = 0 ; i < waves.size() ; i++)
+        {
+            waves[i].update();
+            waves[i].draw(window);
+        }
 
-		waves.erase(std::remove_if(waves.begin(),
-			waves.end(),
-			[](auto& elem) { return elem.isDead(); }),
-			waves.end());
+        waves.erase(std::remove_if(waves.begin(),
+                                   waves.end(),
+                                   [](auto& elem) { return elem.isDead(); }),
+                    waves.end());
 
-		window.display();
+        window.display();
 
-		sf::sleep(sf::milliseconds(10));
-
-	}
+        sf::sleep(sf::milliseconds(10));
+    }
 
 
     return 0;
 }
-
-
