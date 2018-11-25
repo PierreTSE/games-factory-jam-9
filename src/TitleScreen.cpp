3cb0e2fead1567bdcad15f10efc9065bca457497
Theo Escobar <escobar.theo@yahoo.fr>
2018-11-26 00:28:28 +0100

    tenez ceci est mon foutre

--- src/TitleScreen.cpp
+++ src/TitleScreen.cpp
@@ -46,7 +46,7 @@ std::unique_ptr<Screen> TitleScreen::execute()
     continu = fade();
     if(!continu)
         return std::unique_ptr<Screen>(nullptr);
-    return std::make_unique<LevelScreen>(window_, 5);
+    return std::make_unique<LevelScreen>(window_, 1);
 }
 
 TitleScreen::TitleScreen(sf::RenderWindow& window) : Screen(window)
